9a270d7f7c0aac08004f24c57ba407951d1163bd
0x416c616e <29168822+0x416c616e@users.noreply.github.com>
2020-01-25 17:16:26 -0600

    starting to build clone_all.py, added support for getting username from sys.argv

--- clone_all.py
+++ clone_all.py
@@ -1,1 +1,23 @@
-print("not done")
+#!/usr/bin/env python3
+#Clone All
+#Clone all repos from a specific user on GitHub
+
+import sys
+
+def main():
+    if (len(sys.argv) != 2):
+        print("Error with number of command line args.")
+        print("Here's how you use this program:")
+        print("clone_all.py username_goes_here")
+        sys.exit(1)
+    else:
+        print("Correct number of command line args.")
+        username = sys.argv[1]
+        print("Username: " + username)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nQuitting. Goodbye.")
+        sys.exit()
