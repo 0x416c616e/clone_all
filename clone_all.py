623e87d565daf8d365f333d2d7c2bc07241c97d1
0x416c616e <29168822+0x416c616e@users.noreply.github.com>
2020-01-25 17:19:55 -0600

    finish making url for first repo tab page

--- clone_all.py
+++ clone_all.py
@@ -5,6 +5,8 @@
 import sys
 
 def main():
+    #getting command line args
+    username = ""
     if (len(sys.argv) != 2):
         print("Error with number of command line args.")
         print("Here's how you use this program:")
@@ -14,6 +16,13 @@ def main():
         print("Correct number of command line args.")
         username = sys.argv[1]
         print("Username: " + username)
+    #proceeding with program now that cli arg has been finished
+
+    #building URL of initial repo tab page
+    #example: https://github.com/0x416c616e?tab=repositories
+    first_repo_page_url = "https://github.com/" + username + "?tab=repositories"
+    print("URL: " + first_repo_page_url)
+
 
 if __name__ == '__main__':
     try:
